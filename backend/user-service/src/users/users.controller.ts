df575413ce9d918c13c1cdaf00996055b79b9fa5
nonamebruhyes <sdaniel9391@gmail.com>
2026-04-24 10:30:04 +0200

    Nueva estructura |Microservicios| new Api-gateway

--- backend/user-service/src/users/users.controller.ts
+++ backend/user-service/src/users/users.controller.ts
@@ -1,23 +1,24 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @MessagePattern('create_user')
+  create(@Payload() data: CreateUserDto) {
+    return this.usersService.create(data);
   }
 
-  @Get(':id')
-  findById(@Param('id') id: string) {
-    return this.usersService.findById(+id);
+  @MessagePattern('find_user_by_id')
+  findById(@Payload() id: number) {
+    return this.usersService.findById(id);
   }
 
-    @Get(':email')
+  @MessagePattern('find_user_by_email')
   findByEmail(@Param('email') email: string) {
     return this.usersService.findByEmail(email);
   }
